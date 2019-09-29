a73cbbccbeae45436d28e65be0925a547b853b8a
Keijo Tomiste <tomiste.keijo@gmail.com>
2019-09-29 17:46:22 +0300

    data for landing

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -5,11 +5,4 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class LandingComponent {}
